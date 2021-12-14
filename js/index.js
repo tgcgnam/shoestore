a3c015492a98b61f215e2c7dc19a29e66271589d
Trương Công Nam <tgcgnam@Nam-Mac-Pro.local>
2021-12-14 17:43:22 +0700

    push

--- js/index.js
+++ js/index.js
@@ -7,3 +7,9 @@ for (let i = 0; i < card__like.length; i++) {
     };
 
 }
+$('.slide__banner').slick({
+    dots: true,
+    prevArrow: `<button class="slick-prev slick-arrow" aria-label="prev" type="button" style="display: block;"><i class='bx bx-chevron-left'></i></button>`,
+    nextArrow: `<button class="slick-next slick-arrow" aria-label="Next" type="button" style="display: block;"><i class='bx bx-chevron-right' ></i></button>`,
+    // appendDots: `<button type="button" role="tab" id="slick-slide-control00" aria-controls="slick-slide00" aria-label="1 of 2" tabindex="0" aria-selected="true"><i class='bx bx-minus'></i></button>`,
+});
