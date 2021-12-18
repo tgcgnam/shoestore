769dd6ad8de1ffeca03b2102a648e03676e6f02c
Trương Công Nam <tgcgnam@Nam-Mac-Pro.local>
2021-12-18 15:46:44 +0700

    push

--- js/index.js
+++ js/index.js
@@ -11,5 +11,4 @@ $('.slide__banner').slick({
     dots: true,
     prevArrow: `<button class="slick-prev slick-arrow" aria-label="prev" type="button" style="display: block;"><i class='bx bx-chevron-left'></i></button>`,
     nextArrow: `<button class="slick-next slick-arrow" aria-label="Next" type="button" style="display: block;"><i class='bx bx-chevron-right' ></i></button>`,
-    // appendDots: `<button type="button" role="tab" id="slick-slide-control00" aria-controls="slick-slide00" aria-label="1 of 2" tabindex="0" aria-selected="true"><i class='bx bx-minus'></i></button>`,
 });
